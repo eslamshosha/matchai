e7bdd059daad12d697a03d1da8093e8bde904d80
eslam shosha <you@example.comeslamshosha22.es@gmail.com>
2022-05-15 15:18:29 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,156 @@
+let hint = document.querySelector(".preloader");
+window.onload = function(){
+  //hide the preloader
+  setTimeout(function(){
+    hint.style.display = 'none';
+  },700);
+}
+
+$(document).ready(function () {
+  new WOW().init();
+  // for counter //
+
+  var a = 0;
+  $(window).scroll(function () {
+    var oTop = $(".counter-cont").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+      $(".counter-num").each(function () {
+        var $this = $(this),
+          countTo = $this.attr("data-count");
+        $({
+          countNum: $this.text(),
+        }).animate(
+          {
+            countNum: countTo,
+          },
+
+          {
+            duration: 2000,
+            easing: "swing",
+            step: function () {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+              $this.text(this.countNum);
+              //alert('finished');
+            },
+          }
+        );
+      });
+      a = 1;
+    }
+  });
+
+  // end counter //
+
+  //phone size menu onclick
+  if ($(window).width() <= 1199) {
+    $("#menu-id").click(function (e) {
+      e.preventDefault();
+      $(".overlay-box").fadeToggle(300);
+      $(".navgition").toggleClass("reset-left");
+      $("body").toggleClass("overflow");
+    });
+    $(".nav-head .close-btn, .overlay-box, .big-menu li a").click(function () {
+      $(".overlay-box").fadeOut(300);
+      $(".navgition").removeClass("reset-left");
+      $(".menu-bars .bars").toggleClass("open-bars");
+      $(".menu-bars .bars").toggleClass("close-bars");
+      $("body").removeClass("overflow");
+    });
+    // $(".lang-word, .chevron-down").click(function (e) {
+    //   e.preventDefault();
+    //   var item = $(this).siblings(".dropdown-content");
+    //   item.slideToggle(400);
+    // });
+  }
+  $("#menu-id").click(function () {
+    $(".menu-bars .bars").toggleClass("open-bars");
+    $(".menu-bars .bars").toggleClass("close-bars");
+  });
+  //fixed nav
+  $stickyNav = $(".top-header");
+  $(window).on("scroll load", function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 200) {
+      $stickyNav.addClass("fixed-nav", 500);
+    } else {
+      $stickyNav.removeClass("fixed-nav", 500);
+    }
+    if (scroll == 0) {
+      $stickyNav.removeClass("fixed-nav", 500);
+    }
+  });
+  var $stickyheader = $("header");
+  lastScroll = 0;
+  $(window).on("scroll load", function () {
+    var scroll = $(window).scrollTop();
+    if (lastScroll - scroll > 0) {
+      $stickyheader.addClass("fixed-header", { duration: 1000 });
+    } else {
+      $stickyheader.removeClass("fixed-header", { duration: 500 });
+    }
+    lastScroll = scroll;
+    if (scroll == 0) {
+      $stickyheader.removeClass("fixed-header", { duration: 500 });
+    }
+  });
+  ///////// ** main** /////////
+  var specials = new Swiper(".main-slider .swiper-container", {
+    loop: true,
+    autoplay: true,
+    pagination: {
+      el: ".main-slider .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".main-slider .swiper-btn-next",
+      prevEl: ".main-slider .swiper-btn-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      767: {
+        slidesPerView: 1,
+      },
+      992: {
+        slidesPerView: 1,
+      },
+      1199: {
+        slidesPerView: 1,
+      },
+    },
+  });
+  ///////// **news-section** /////////
+  var screen = new Swiper(".news-section .swiper-container", {
+    loop: true,
+    autoplay: true,
+    pagination: {
+      el: ".news-slider .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".news-section .swiper-btn-next",
+      prevEl: ".news-section .swiper-btn-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+      767: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      1199: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
+});
